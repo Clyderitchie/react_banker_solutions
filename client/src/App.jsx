21ca26a906830ede53ae371680d4a1180737b9bd
francis <clyderitchie@yahoo.com>
2024-10-07 20:30:30 -0700

    adding in dev dep's

---
+++ client/src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import { ToggleLeft } from 'lucide-react'
+import { MessageCircle } from 'lucide-react'
+
+function App() {
+
+  return (
+    <>
+        <ToggleLeft/>
+        <MessageCircle/>
+        
+    </>
+  )
+}
+
+export default App
